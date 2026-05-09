ba4d3e8249a2fc9fdbc93b3143b11a78a629f2e6
SrViniih <77179612+SrViniih@users.noreply.github.com>
2026-05-09 11:59:45 +0200

    att

---
+++ dados.js
@@ -0,0 +1,52 @@
+const categorias = [
+    { 
+        id: 'desayunos', 
+        nome: 'Desayunos', 
+        itens: ['Café Solo', 'Café con Leche', 'Tostada con Tomate', 'Croissant Artesano', 'Huevos Revueltos', 'Zumo de Naranja', 'Tortilla Española', 'Yogur con Frutas', 'Chocolate con Churros', 'Pancakes Taramundi', 'Muffins Caseros', 'Tostada con Jamón', 'Bocadillito Caliente', 'Fruta de Temporada', 'Té Matcha', 'Desayuno Especial'] 
+    },
+    { 
+        id: 'tablas', 
+        nome: 'Tablas Completas', 
+        itens: ['Tabla Ibérica', 'Tabla de Quesos', 'Tabla Mixta Taramundi'] 
+    },
+    { 
+        id: 'platos', 
+        nome: 'Platos Combinados', 
+        itens: ['Plato del Día', 'Combinado de Lomo', 'Pollo Asado con Patatas', 'Entrecot con Guarnición'] 
+    },
+    { 
+        id: 'sartenes', 
+        nome: 'Sartenes Taramundi', 
+        itens: ['Sartén de Gulas', 'Sartén Asturiana', 'Sartén de Chorizo', 'Sartén de la Casa', 'Sartén Vegetariana', 'Sartén de Pulpo', 'Sartén de Setas'] 
+    },
+    { 
+        id: 'hamburguesas', 
+        nome: 'Hamburguesas', 
+        itens: ['Clásica', 'Queso Extrema', 'Taramundi Burger', 'Veggie Delight', 'Bacon Lover', 'Pollo Crispy', 'Doble Smash', 'BBQ Special', 'Hambúrguer de la Abuela'] 
+    },
+    { 
+        id: 'raciones', 
+        nome: 'Raciones', 
+        itens: ['Patatas Bravas', 'Calamares', 'Croquetas Caseras', 'Pimientos Padrón', 'Alitas de Pollo', 'Gambas al Ajillo', 'Ensaladilla Rusa', 'Nachos Taramundi'] 
+    },
+    { 
+        id: 'bocatas', 
+        nome: 'Bocatas', 
+        itens: ['Serranito', 'Bocata de Calamares', 'Lomo con Queso', 'Bocata Vegano', 'Pepito de Ternera', 'Bocata de Tortilla', 'Especial Taramundi'] 
+    },
+    { 
+        id: 'sandwich', 
+        nome: 'Sandwich y Perritos', 
+        itens: ['Sandwich Mixto', 'Sandwich Club', 'Hot Dog Clásico', 'Hot Dog Especial', 'Vegetal Sandwich'] 
+    },
+    { 
+        id: 'ensaladas', 
+        nome: 'Ensaladas de la Casa', 
+        itens: ['César', 'Mixta', 'Ensalada Taramundi'] 
+    },
+    { 
+        id: 'postres', 
+        nome: 'Postres', 
+        itens: ['Tarta de Queso', 'Flan Casero'] 
+    }
+];
